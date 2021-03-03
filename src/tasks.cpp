b1255522d42a03478967b5f3965fe7f8fdb43320
Sungatullin Bulat <sungatullinbulat05@gmail.com>
2021-03-03 23:45:13 +0300

    init

--- src/tasks.cpp
+++ src/tasks.cpp
@@ -11,35 +11,79 @@ using std::copy;
 
 // Задание 1
 void swap_args(int *lhs, int *rhs) {
-    // напишите код здесь ...
+    if ((*lhs != 0) || (*rhs != 0)){
+        int t = *lhs;
+        *lhs = *rhs;
+        *rhs = t;
+    }
 }
 
 // Задание 2
 int **allocate_2d_array(int num_rows, int num_cols, int init_value) {
-    // напишите код здесь ...
-    return nullptr;
+    if ((num_rows > 0) && (num_cols > 0)){
+        int **arr = new int *[num_rows];
+        for (int row_index = 0; row_index < num_rows; row_index++) {
+            arr[row_index] = new int[num_cols];
+        }
+        for (int row_index = 0; row_index < num_rows; row_index++) {
+            for (int column_index = 0; column_index < num_cols; column_index++) {
+                arr[row_index][column_index] = init_value;
+            }
+        }
+        return arr;
+    }
+    else{
+        return nullptr;
+    }
 }
 
 // Задание 3
 bool copy_2d_array(int **arr_2d_source, int **arr_2d_target, int num_rows, int num_cols) {
-    // напишите код здесь ...
+    if ((num_rows > 0) && (num_cols > 0) && (arr_2d_source != nullptr) && (arr_2d_target != nullptr)){
+        for (int i = 0; i < num_rows; i++){
+            for (int j = 0; j < num_cols; j++){
+                arr_2d_target[i][j] = arr_2d_source[i][j];
+            }
+        }
+        return true;
+    }
     return false;
 }
 
 // Задание 4
 void reverse_1d_array(vector<int> &arr) {
-    // напишите код здесь ...
+    for (int i = 0; i < arr.size()/2; i++){
+        int x = arr[i];
+        arr[i] = arr[arr.size() - i - 1];
+        arr[arr.size() - i - 1] = x;
+    }
 }
 
 // Задание 5
 void reverse_1d_array(int *arr_begin, int *arr_end) {
-    // напишите код здесь ...
+    if ((arr_begin != nullptr) && (arr_end != nullptr)){
+        for (int i = 0; i <= (arr_end - arr_begin); i++){
+            int t = *(arr_begin + i);
+            *(arr_begin + i) = *(arr_end - i);
+            *(arr_end - i) = t;
+        }
+    }
 }
 
 // Задание 6
 int *find_max_element(int *arr, int size) {
-    // напишите код здесь ...
-    return nullptr;
+    if ((arr != nullptr) && (size > 0)){
+        int *max;
+        max = &arr[0];
+        for (int i = 0; i < size; i++) {
+            if (arr[i] > *max) {
+                max = &arr[i];
+            }
+        }
+        return max;
+    } else{
+        return nullptr;
+    }
 }
 
 // Задание 7
